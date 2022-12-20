70de18b039132ce22ed15426911c3fa2962fddbc
Rishi Gautam <gautamrishi7100@gmail.com>
2022-12-21 02:38:29 +0530

    Task-7
    Dog Year Commit

---
+++ Task-7/Dog_year.js
@@ -0,0 +1,13 @@
+const myAge = 30;
+//early Years 
+let earlyYears= 2;
+earlyYears *= 10.5;
+let laterYears = myAge-2;
+laterYears *=4;
+
+console.log(laterYears);
+console.log(earlyYears);
+
+let myAgeInDogYear = earlyYears+laterYears;
+let myName= "technocolabs".toUpperCase();
+console.log(`My name is ${myName}. I am ${myAgeInDogYear} years old in dog years.`);
